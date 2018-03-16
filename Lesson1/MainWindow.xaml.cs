8d8b7ac10efe40bb3e95e2bb6944f0343f3bf59e
Daniel Rużewicz <d.ruzew@gmail.com>
2018-03-16 13:26:12 +0100

    lesson1 -panele - canvas,  grid, uniformgrid, wrappanel, dockpanel, stackpanel, groupbox, expander, scrollviewer

--- Lesson1/MainWindow.xaml.cs
+++ Lesson1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Lesson1
         {
             InitializeComponent();
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("klikles");
+        }
     }
 }
