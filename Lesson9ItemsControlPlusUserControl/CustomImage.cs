f90d8b80c6b78d9b5fbd78fc230c1abc5b79774a
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-14 12:12:54 +0200

    obrazki do folderu + stworzenie klasy CustomImage

---
+++ Lesson9ItemsControlPlusUserControl/CustomImage.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Lesson9ItemsControlPlusUserControl
+{
+    public class CustomImage
+    {
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public bool IsFound { get; set; }
+
+        public CustomImage()
+        {
+            ImagePath = Path.Combine(Directory.GetCurrentDirectory(), "Images", ImagePath);
+            Description = "Default";
+        }
+
+        public CustomImage(string imagePath, string description)
+        {
+            ImagePath = imagePath;
+            Description = description;
+            IsFound = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Images", ImagePath));
+        }
+    }
+}
