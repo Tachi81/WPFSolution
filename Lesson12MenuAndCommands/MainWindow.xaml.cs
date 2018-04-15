cadc65927a6ccf9f8207437cb3fa5d004b23bce6
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-15 12:54:59 +0200

    Dodanie komend ikon do nich, skrótów, wybierania z altem (_ A) , działania (command)

--- Lesson12MenuAndCommands/MainWindow.xaml.cs
+++ Lesson12MenuAndCommands/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lesson12MenuAndCommands
 {
@@ -22,6 +10,8 @@ namespace Lesson12MenuAndCommands
     {
         public MainWindow()
         {
+            ApplicationCommands.Copy.InputGestures.Clear();
+            ApplicationCommands.Copy.InputGestures.Add(new KeyGesture(Key.H, ModifierKeys.Control));
             InitializeComponent();
         }
     }
