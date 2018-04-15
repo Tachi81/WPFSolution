a733c888a4d374696dbd24e2d5c8a11948e3a06b
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-15 13:33:54 +0200

    dodnie własnej customowej metody EXIT

---
+++ Lesson12MenuAndCommands/CustomCommands.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace Lesson12MenuAndCommands
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand("Exit", "Exit",
+            typeof(CustomCommands), new InputGestureCollection()
+            {
+                new KeyGesture(Key.W,ModifierKeys.Control)
+            });
+    }
+}
